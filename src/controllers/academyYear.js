78f851f9f06a7ecec41ce4d5fafbcf43a1618235
Luis Marcano <lmarcano203@gmail.com>
2023-07-11 09:06:37 -0500

    erasing comments

--- src/controllers/academyYear.js
+++ src/controllers/academyYear.js
@@ -5,11 +5,6 @@ const newYearController = async (req, res, next) => {
 
   const { newYear, initial, elementary, high } = req.body;
 
-  //initial, elementary y high como objectos
-
-  //? EDUCACION INICIAL  [ARR {Obj}]
-  //? PRIMER GRADO, SEGUNDO GRADO, TERCER GRADO, CUARTO GRADO, QUINTO GRADO, SEXTO GRADO [ ARR { objeto }]
-  //? PRIMER AÑO, SEGUNDO AÑO, TERCER AÑO, CUARTO AÑO, QUINTO AÑO, SEXTO AÑO  [ARR { obj }]
   try{
 
     const searchYear = await Year.findOne({
@@ -26,106 +21,16 @@ const newYearController = async (req, res, next) => {
       }
     )
 
-    //FORMATO
-    /* {gradeName: "Primer Grado", sections: ["A", "B", "C"]} */
-
     //Eduación inicial
-    if(initial.length > 0){
-
-      await gradeAssignment(createYear.id, initial);
-
-      /* for(let grade of initial){
-
-        const newInitial = await Grade.create({
-          grade: grade.gradeName
-        })
-  
-        await newInitial.setYear(createYear.id);
-
-        for(let section of grade.sections){
-
-          const newSection = await Section.create({
-            sectionName: section
-          });
-
-          await newSection.setGrade(newInitial.id);
-
-        }
-
-      } */
-
-
-    }
+    if(initial.length > 0) await gradeAssignment(createYear.id, initial);
 
     //Primaria
-    if(elementary.length > 0){
+    if(elementary.length > 0) await gradeAssignment(createYear.id, elementary);
 
-      await gradeAssignment(createYear.id, elementary);
+    //Segundaria
+    if(high.length > 0) await gradeAssignment(createYear.id, high);
 
-    /*   for(let grade of elementary){
-        
-        const newGrade = await Grade.create({
-          grade: grade.gradeName
-        });
-
-        await newGrade.setYear(createYear.id);
-
-        for(let section of grade.sections){
-
-          const newSection = await Section.create({
-            sectionName: section
-          });
-
-          await newSection.setGrade(newGrade.id)
-
-        }
-
-      } */
-
-    }
-
-    if(high.length > 0){
-
-      await gradeAssignment(createYear.id, high)
-
-     /*  for(let grade of high){
-        
-        const newGrade = await Grade.create({
-          grade: grade.gradeName
-        });
-
-        await newGrade.setYear(createYear.id);
-
-        for(let section of grade.sections){
-
-          const newSection = await Section.create({
-            sectionName: section
-          });
-
-          await newSection.setGrade(newGrade.id)
-
-        }
-
-      } */
-
-    }
-    
-    
-    const test = await Year.findByPk(createYear.id, {
-      include: [
-        {
-          model: Grade,
-          include: [
-            {
-              model: Section
-            }
-          ]
-        }
-      ]
-    })
-    return res.status(200).json(test)
-
-    //return res.status(200).json("El nuevo año escolar fue creado exitosamente!");
+    return res.status(200).json("El nuevo año escolar fue creado exitosamente!");
 
   }catch(err){
     console.log(err)
