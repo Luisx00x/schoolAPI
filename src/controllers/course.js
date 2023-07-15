f342a2ea20d98fc62bf0aa72d4daf9d06921e31d
Luis Marcano <lmarcano203@gmail.com>
2023-07-14 21:07:29 -0500

    modifiying course controller to register course correctly

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -1,14 +1,18 @@
-const { Course, Section, Teacher, Grade, Student } = require('../db.js');
+const { Course, Section, Teacher, Grade, Schedules } = require('../db.js');
 
 const createCourse = async (req, res, next) => {
 
-  const { courseName, init, end, gradeId, teacherId, sectionId} = req.body;
+  const { 
+    courseName,  
+    gradeId, 
+    days,   //Array de objetos, una posicion por dia, un objeto con dia, y horas
+    teacherId, 
+    sectionId,
+  } = req.body;
 
   try{
 
     if(!courseName) return res.status(400).json("No ha ingresado un nombre para el curso");
-    if(!init) return res.status(400).json("No ha seleccionado una hora de inicio para el curso");
-    if(!end) return res.status(400).json("No se ha seleccionado una una de finalización para el curso");
     if(!teacherId) return res.status(400).json("No se ha especificado un profesor");
     if(!sectionId) return res.status(400).json("No se ha especificado una sección");
 
@@ -24,21 +28,24 @@ const createCourse = async (req, res, next) => {
               model: Grade
             }
           ]
+        },
+        {
+          model: Schedules
         }
       ]
     });
 
+    
     //* REQUIERE UNA SECCIÓN PARA COMPARAR CON EL NOMBRE DEL GRADO QUE RECIBE
     const valiadteCourse = searchCourse.find( ele => {
+      /* console.log(ele.Section.GradeId) */
       if(ele.Section) return ele.Section.GradeId === gradeId
     })
-
+    
     if(valiadteCourse) return res.status(200).json({msg: "El Curso ya existe", valiadteCourse});
 
     const createCourse = await Course.create({
-      courseName,
-      init,
-      end
+      courseName
     });
 
     // Se busca al profesor ingresado
@@ -49,8 +56,17 @@ const createCourse = async (req, res, next) => {
     //Se agrega el curso a una sección
 
     const searchSection = await Section.findByPk(sectionId);
+
     //Se le agrega la seccion /=> Los alumnos estan en la seccion
     await searchSection.setCourse(createCourse.id);
+
+    days.map(async day => {
+      
+      const newDayAssign = await Schedules.create(day)
+
+      newDayAssign.setCourse(createCourse.id);
+
+    })
 
     return res.status(200).json({msg: "Se ha agregado la sección exitosamente!", createCourse});
 
