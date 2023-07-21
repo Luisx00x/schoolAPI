bea689707528a0b4ebff65c05520d1ecfbfb21fe
Luis Marcano <lmarcano203@gmail.com>
2023-07-21 18:11:01 -0500

    make the homework upload controller the assosiation between Homework with Teacher and Course

--- src/controllers/homeworks.js
+++ src/controllers/homeworks.js
@@ -1,11 +1,27 @@
-
+const { Homework } = require('../db.js');
 
 const uploadHomework = async (req, res, next) => {
   
   try{
 
-    const {filename} = req.file
-    console.log(req.body)
+    const { filename } = req.file
+    const { title } = req.body;
+    const teacherId = parseInt(req.body.teacherId);
+    const courseId = parseInt(req.body.courseId)
+
+    if(!filename) return res.status(400).json("Falta un archivo");
+    if(!title) return res.status(400).json("Falta el titulo de la asignación");
+    if(!teacherId) return res.status(400).json("Falta un profesor");
+    if(!courseId) return res.status(400).json("Falta un curso");
+
+    const newHomework = await Homework.create({
+      asignation: title,
+      location: filename
+    });
+
+    newHomework.setTeacher(teacherId);
+    newHomework.setCourse(courseId);
+
 
     res.status(200).json("TODO OK");
     next()
