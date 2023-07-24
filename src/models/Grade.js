aff84ed8ea1f5520cedd5989bdcad90decb8e788
Luisx00x <lmarcano203@gmail.com>
2023-07-24 08:54:53 -0500

    Changing Grade model: adding level attribute as ENUM to storage education level of the grade

--- src/models/Grade.js
+++ src/models/Grade.js
@@ -2,6 +2,10 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Grade', {
+    level: {
+      type: DataTypes.ENUM(['Inicial','Primaria','Secundaria']),
+      allowNull: false
+    },
     grade: {
       type: DataTypes.STRING,
       allowNull: false
