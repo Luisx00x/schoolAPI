39af2f16a57dccdd4427e2bfefdf9fc9364f49e4
Luisx00x <lmarcano203@gmail.com>
2023-07-29 10:36:39 -0500

    now the route can create students releases as well

---
+++ src/controllers/studentReleases.js
@@ -0,0 +1,29 @@
+const { Student, Teacher, StudentReleases } = require('../db.js');
+
+const findOneStudent = async (req, res, next) => {
+
+  try{
+
+    const { studentId } = req.query;
+
+    if(!studentId) return res.status(400).json("Falta un estudiante para buscar");
+
+    if(!findOneStudent) return res.status(400).json("El alumno que busca no existe");
+
+    const findReleases = await StudentReleases.findAll({
+      where: {
+        StudentId: studentId
+      }
+    })
+
+    return res.status(200).json(findReleases);
+
+  }catch(err){
+    next(err);
+  }
+
+}
+
+module.exports = {
+  findOneStudent
+}
