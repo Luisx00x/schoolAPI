c88a937dc52e8828f964d816da1a4b022809f127
Luisx00x <lmarcano203@gmail.com>
2023-07-29 10:37:41 -0500

    now the route can create students releases as well

--- src/controllers/SectionReleases.js
+++ src/controllers/SectionReleases.js
@@ -1,42 +1,77 @@
-const { SectionReleases, Teacher, Section } = require('../db.js');
+const { SectionReleases, StudentReleases, Teacher, Section, Student } = require('../db.js');
 
 const createRelease = async (req, res, next) => {
 
   try{
 
-    const { userRol, sender, title, sectionId } = req.body;
-
+    const { userRol, sender, title, sectionId, studentId } = req.body;
     const {filename} = req.file;
 
     if(!userRol) return res.status(400).json("El solicitante no es un usuario");
     if(!title) return res.status(400).json("Falta ingresar un titulo");
-    if(!sectionId) return res.status(400).json("Falta la sección destino");
     if(!sender) return res.status(400).json("Falta especificar quien envia");
 
-    //Si el enviante es un profesro
-    if(userRol == 3){
+    if(sectionId){
 
-      const teacherInfo = await Teacher.findByPk(sender);
+      if(userRol == 3){
+  
+        const teacherInfo = await Teacher.findByPk(sender);
+  
+        if(!teacherInfo) return res.status(400).json("No hay registros de ese profesor");
+  
+        const searchSection = await Section.findByPk(sectionId);
+  
+        if(!searchSection) return res.status(400).json("No existe esa sección");
+  
+        const senderValue = `${teacherInfo.name} ${teacherInfo.lastName}`;
+  
+        const newRelease = await SectionReleases.create({
+          title,
+          sender: senderValue,
+          location: filename
+        })
+  
+        //Conexion con sectionID para hacer las queries
+        await newRelease.setSection(sectionId);
+  
+        return res.status(200).json("TODO OK")
+      }
 
-      if(!teacherInfo) return res.status(400).json("No hay registros de ese profesor");
+      //TODO AQUI VA SI ES ADMIN
 
-      const searchSection = await Section.findByPk(sectionId);
-
-      if(!searchSection) return res.status(400).json("No existe esa sección");
-
-      const senderValue = `${teacherInfo.name} ${teacherInfo.lastName}`;
-
-      const newRelease = await SectionReleases.create({
-        title,
-        sender: senderValue,
-        location: filename
-      })
-
-      //Conexion con sectionID para hacer las queries
-      await newRelease.setSection(sectionId);
-
-      return res.status(200).json("TODO OK")
     }
+
+    if(studentId){
+
+      if(userRol == 3){
+
+        const teacherInfo = await Teacher.findByPk(sender);
+  
+        if(!teacherInfo) return res.status(400).json("No hay registros de ese profesor");
+  
+        const searchSection = await Student.findByPk(studentId);
+  
+        if(!searchSection) return res.status(400).json("No existe esa sección");
+  
+        const senderValue = `${teacherInfo.name} ${teacherInfo.lastName}`;
+  
+        const newRelease = await StudentReleases.create({
+          title,
+          sender: senderValue,
+          location: filename
+        })
+  
+        //Conexion con sectionID para hacer las queries
+        await newRelease.setStudent(studentId);
+  
+        return res.status(200).json("TODO OK")
+      }
+
+    }
+    
+    if(!sectionId) return res.status(400).json("Falta la sección destino");
+
+    //Si el enviante es un profesro
 
 
     return res.status(200).json("todo ok");
