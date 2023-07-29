c63f84e9ac581ecdbb31fc353b6fe2222462123f
Luisx00x <lmarcano203@gmail.com>
2023-07-29 11:51:11 -0500

    adding a route to search Parent releases

--- src/routes/releases.js
+++ src/routes/releases.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { searchReleases } = require('../controllers/SectionReleases');
 const { findOneStudent } = require('../controllers/studentReleases');
+const { findParentReleases } = require('../controllers/parentsReleases');
 
 const router = Router();
 
 router.get('/searchSection', searchReleases);
 router.get('/findStudentReleases', findOneStudent);
+router.get('/findParentReleases', findParentReleases);
 
 module.exports = router;
