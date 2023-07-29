63a55a705a08de47cb9476764a2ec77f966fa709
Luisx00x <lmarcano203@gmail.com>
2023-07-29 10:38:24 -0500

    adding a route to find releases of one student

--- src/routes/releases.js
+++ src/routes/releases.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { createRelease, searchReleases } = require('../controllers/SectionReleases');
+const { searchReleases } = require('../controllers/SectionReleases');
+const { findOneStudent } = require('../controllers/studentReleases');
 
 const router = Router();
 
 router.get('/searchSection', searchReleases);
+router.get('/findStudentReleases', findOneStudent);
 
 module.exports = router;
