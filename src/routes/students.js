998e782e8c9d038a6c747744e90527e5a3b01bed
Luisx00x <lmarcano203@gmail.com>
2023-08-02 14:38:02 -0500

    adding studentCourses route

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { assignStudents, searchSectionStudents, findAllStudents, findStudentSection, findStudentInfo, findCalifications } = require('../controllers/students.js');
+const { assignStudents, searchSectionStudents, findAllStudents, findStudentSection, findStudentInfo, findCalifications, findStudentCourses } = require('../controllers/students.js');
 const router = Router();
 
 router.get('/searchSectionStudents', searchSectionStudents);
@@ -8,5 +8,6 @@ router.get('/searchAllStudents', findAllStudents);
 router.get('/findStudentSection', findStudentSection);
 router.get('/studentInformation', findStudentInfo);
 router.get('/studentCalifications', findCalifications);
+router.get('/studentCourses', findStudentCourses);
 
 module.exports = router;
