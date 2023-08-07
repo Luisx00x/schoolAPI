3e79225d18cb9acacf38bb146dbf796b4322c902
Luisx00x <lmarcano203@gmail.com>
2023-08-07 15:57:13 -0500

    adding routes to search homeworksAnswers and update its

--- src/controllers/homeworks.js
+++ src/controllers/homeworks.js
@@ -98,6 +98,8 @@ const uploadAnswer = async (req, res, next) => {
 
   try{
 
+    if(!req.file) return res.status(404).json("No se encuentra el archivo")
+
     const { filename } = req.file;
 
     const { studentId, homeworkId } = req.body;
@@ -113,16 +115,20 @@ const uploadAnswer = async (req, res, next) => {
     
     if(!findHomework) return res.status(400).json("NO existe esa tarea");
 
-    const answerPlace = await HomeworksAnswer.findAll()
+    const answerPlace = await HomeworksAnswer.findOne({
+      where: {
+        HomeworkId: homeworkId,
+        StudentId: studentId
+      }
+    })
 
-    /* const newAnswer = await HomeworksAnswer.create({
+    if(!answerPlace) return res.status(400).json("El estudiante no tiene esta tarea asignada")
+
+    await answerPlace.update({
       location: filename
-    }) */
+    })
 
-   /*  await newAnswer.setStudent(findStudent.id);
-    await newAnswer.setHomework(findHomework.id); */
-
-    return res.status(200).json(answerPlace);
+    return res.status(200).json("La respuesta se ha subido correctamente");
 
   }catch(err){
     next(err);
@@ -143,10 +149,72 @@ const getAnswers = async (req, res, next) => {
 
 }
 
+const studentsHomeworks = async (req, res, next) => {
+
+  try{
+
+    const { homeworkId } = req.query;
+
+    if(!homeworkId) return res.status(400).json("No se ha especificado una tarea");
+
+    const findHomework = await Homework.findByPk(homeworkId,{
+      include: [
+        {
+          model: HomeworksAnswer,
+          include: [
+            {
+              model: Student,
+              attributes: ["DNI","names","fatherLastName","motherLastName"]
+            }
+          ]
+        }
+      ]
+    });
+
+    if(!findHomework) return res.status(400).json("La tarea buscada no existe");
+
+    
+    return res.status(200).json(findHomework);
+
+
+
+  }catch(err){
+    next(err);
+  }
+
+}
+
+const homeworkCalification = async (req, res, next) => {
+
+  try{
+
+    const { homeworkAnswerId, calification } = req.body
+
+    if(!homeworkAnswerId) return res.status(400).json("No se ha establecido una tarea");
+    if(!calification) return res.status(400).json("No se asignó una calificaición");
+
+    const findHomeworkAnswer = await HomeworksAnswer.findByPk(homeworkAnswerId);
+
+    if(!findHomeworkAnswer) return res.status(400).json("No hay registros de la tarea");
+
+    await findHomeworkAnswer.update({
+      calification
+    })
+
+    return res.status(200).json({title: "Asignación de Calificación", msg: "Calificación subida correctamente"});
+
+  }catch(err){
+    next(err);
+  }
+
+} 
+
 
 module.exports = {
   uploadHomework,
   getHomeworks,
   uploadAnswer,
-  getAnswers
+  getAnswers,
+  studentsHomeworks,
+  homeworkCalification
 }
--- src/routes/homeworks.js
+++ src/routes/homeworks.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { getHomeworks, getAnswers } = require('../controllers/homeworks');
+const { getHomeworks, getAnswers, studentsHomeworks, homeworkCalification } = require('../controllers/homeworks');
 const router = Router();
 
 router.get('/getHomeworks', getHomeworks);
 router.get('/getAnswers', getAnswers);
+router.get('/homeworksAnswers', studentsHomeworks);
+router.post('/homeworkCalification', homeworkCalification);
 
 module.exports = router;
